b65875f2d81debdad9b9710510c571d3b7d3d895
Timdows <tim@inexpro.nl>
2018-12-27 17:27:56 +0100

    Basic setup of project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace ICSBirthdayValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Missing complete url to ics file");
+            }
+
+            var icsUrl = args[0];
+
+            using (var client = new WebClient())
+            {
+                using (var stream = new MemoryStream(client.DownloadData(icsUrl)))
+                {
+                    
+
+                }
+            }
+        }
+    }
+}
