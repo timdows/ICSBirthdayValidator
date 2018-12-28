b46d04c73b1c9e697d809a59a711a53c47afa66a
Timdows <tim@inexpro.nl>
2018-12-28 16:02:16 +0100

    Finishing up code

--- Program.cs
+++ Program.cs
@@ -1,28 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
 
 namespace ICSBirthdayValidator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             if (args.Length != 1)
             {
                 Console.WriteLine("Missing complete url to ics file");
+                return;
             }
 
             var icsUrl = args[0];
 
             using (var client = new WebClient())
             {
-                using (var stream = new MemoryStream(client.DownloadData(icsUrl)))
-                {
-                    
+                var calendarContents = client.DownloadString(icsUrl);
+                var list = new List<string>();
 
+                var calendarEvents = calendarContents.Split("BEGIN:VEVENT");
+
+                foreach (var calendarEvent in calendarEvents)
+                {
+                    var formattedLines = calendarEvent.Split("\r\n").ToList();
+                    var birthdayInformation = new BirthdayInformation();
+                    
+                    birthdayInformation.Recurring = formattedLines.Any(line => line.Contains("FREQ=YEARLY"));
+                    birthdayInformation.Name = formattedLines.SingleOrDefault(item => item.Contains("SUMMARY:"));
+                    birthdayInformation.Date = formattedLines.SingleOrDefault(item => item.Contains("DTSTART;VALUE=DATE:"));
+
+                    Console.WriteLine(birthdayInformation);
                 }
             }
+        }
+    }
+
+    public class BirthdayInformation
+    {
+        public string Name { get; set; }
+        public bool Recurring { get; set; }
+        public string Date { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, recurring: {Recurring} on date: {Date}";
         }
     }
 }
